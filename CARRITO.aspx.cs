d27a32ad0ed434ccb3e0bdc1d029f9ef40282eef
jonatan <jonatan_lazarte1992@hotmail.com>
2024-05-16 16:39:13 -0300

    avances carrito

--- CARRITO.aspx.cs
+++ CARRITO.aspx.cs
@@ -11,15 +11,41 @@ namespace TPWEB_EQUIPO3
 {
     public partial class CARRITO : System.Web.UI.Page
     {
+       
+
         public List<Articulo> ListaArticulos;
+        public List<Articulo> ListaCarrito;
+       
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (Request.QueryString["id"] != null)
+            {
+                int id = int.Parse(Request.QueryString["id"].ToString());
+                ArticuloNegocio negocio = new ArticuloNegocio();
+
+                ListaArticulos = negocio.listar();
+                foreach (Articulo arti in ListaArticulos)
+                {
+                    if (arti.Id == id)
+                    {
+                        List<Articulo> lista = new List<Articulo>();
+                        lista.Add(arti);
+                        
+                       
+                        dgbCarrito.DataSource=lista;
+                        dgbCarrito.DataBind();
+                    }
+
+                }
 
             }
+
+        }
 
           
         
 
     }
+   
 }
--- CARRITO.aspx.designer.cs
+++ CARRITO.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace TPWEB_EQUIPO3
 
     public partial class CARRITO
     {
+
+        /// <summary>
+        /// dgbCarrito control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgbCarrito;
     }
 }
